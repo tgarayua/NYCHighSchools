a077bbaaf7207eb391913b65e9042bf46145fbd5
Thomas Garayua <tgarayua@gmail.com>
2023-07-02 21:10:53 -0400

    Created a placeholder of Search in the searchBar

--- 20230630-TG-NYCSchools/Components/SearchBar.swift
+++ 20230630-TG-NYCSchools/Components/SearchBar.swift
@@ -15,6 +15,7 @@ struct SearchBar: UIViewRepresentable {
     class Coordinator: NSObject, UISearchBarDelegate {
         @Binding var text: String
 
+        // Initialize the coordinator with the binding
         init(text: Binding<String>) {
             _text = text
         }
@@ -22,6 +23,25 @@ struct SearchBar: UIViewRepresentable {
         // Called when the search bar text changes
         func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
             text = searchText // Update the binding with the new search text
+        }
+
+        // Called when the search bar text field begins editing
+        func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+            searchBar.showsCancelButton = true // Show the cancel button
+            searchBar.placeholder = "" // Remove the placeholder text
+        }
+
+        // Called when the search bar text field ends editing
+        func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+            searchBar.showsCancelButton = false // Hide the cancel button
+            searchBar.placeholder = "Search" // Set the placeholder text to "Search"
+        }
+
+        // Called when the cancel button is clicked
+        func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+            searchBar.text = "" // Clear the search bar text
+            searchBar.showsCancelButton = false // Hide the cancel button
+            searchBar.endEditing(true) // End editing of the search bar
         }
     }
 
@@ -34,6 +54,7 @@ struct SearchBar: UIViewRepresentable {
     func makeUIView(context: Context) -> UISearchBar {
         let searchBar = UISearchBar()
         searchBar.delegate = context.coordinator // Set the coordinator as the search bar's delegate
+        searchBar.placeholder = "Search" // Set the initial placeholder text
         return searchBar
     }
 
